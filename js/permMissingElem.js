9cf3570f15514a1c262034c15056fec03109c4b2
Mark Struck <markstruck@comcast.net>
2020-05-04 04:51:02 -0500

    completed 2 lessons

---
+++ js/permMissingElem.js
@@ -0,0 +1,46 @@
+// https://app.codility.com/demo/results/trainingGESCWG-TJD/
+
+// An array A consisting of N different integers is given. 
+// The array contains integers in the range [1..(N + 1)], 
+// which means that exactly one element is missing.
+
+// Your goal is to find that missing element.
+
+// Write a function:
+
+// function solution(A);
+
+// that, given an array A, returns the value of the missing element.
+
+// For example, given array A such that:
+
+//   A[0] = 2
+//   A[1] = 3
+//   A[2] = 1
+//   A[3] = 5
+// the function should return 4, as it is the missing element.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [0..100,000];
+// the elements of A are all distinct;
+// each element of array A is an integer within the range [1..(N + 1)].
+
+function solution(A) {
+    let map = []
+    let index = 1;
+    for(let i = 0; i < A.length + 1; i++) {
+        map[A[i]] = true;
+    }
+
+    while( index < A.length + 1) {
+        if(!map[index]) break;
+        index++;
+    }
+
+    return index;
+}
+
+let a = [2, 3, 1, 5];
+console.log(solution(a));
+
